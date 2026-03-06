eb9a354b576ff39dff881d16e8a4ac11092a79f2
kw554702-ux <kw554702@gmail.com>
2026-03-06 14:29:53 +0000

    Update locations.js

--- assets/js/data/locations.js
+++ assets/js/data/locations.js
@@ -1,8 +1,86 @@
 var locations = [
   {
-    key: "bad-oeynhausen",
-    title: "Bad Oeynhausen — HQ BAOR",
-    coords: [52.206, 8.803],
-    page: "locations/bad-oeynhausen.html"
+    key: "bergen-hohne",
+    title: "Bergen-Hohne",
+    region: "lower-saxony",
+    coords: [52.7819, 9.9273],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "fallingbostel",
+    title: "Fallingbostel",
+    region: "lower-saxony",
+    coords: [52.8664, 9.6956],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "verden",
+    title: "Verden",
+    region: "lower-saxony",
+    coords: [52.9348, 9.2330],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "soltau",
+    title: "Soltau",
+    region: "lower-saxony",
+    coords: [52.9869, 9.8434],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "celle",
+    title: "Celle",
+    region: "lower-saxony",
+    coords: [52.6226, 10.0805],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "nienburg",
+    title: "Nienburg",
+    region: "lower-saxony",
+    coords: [52.6461, 9.2205],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "hanover",
+    title: "Hanover (Hannover)",
+    region: "lower-saxony",
+    coords: [52.3759, 9.7320],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "braunschweig",
+    title: "Braunschweig",
+    region: "lower-saxony",
+    coords: [52.2689, 10.5268],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "hildesheim",
+    title: "Hildesheim",
+    region: "lower-saxony",
+    coords: [52.1508, 9.9510],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "luneburg",
+    title: "Luneburg",
+    region: "lower-saxony",
+    coords: [53.2468, 10.4115],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "hamburg",
+    title: "Hamburg",
+    region: "hamburg",
+    coords: [53.5511, 9.9937],
+    page: "locations/template-location.html"
+  },
+  {
+    key: "lubeck",
+    title: "Lubeck",
+    region: "schleswig-holstein",
+    coords: [53.8655, 10.6866],
+    page: "locations/template-location.html"
   }
 ];
