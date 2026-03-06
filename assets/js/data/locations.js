5fb8530c868f7fc5634130210bc27c6348b4a64c
kw554702-ux <kw554702@gmail.com>
2026-03-06 14:46:55 +0000

    Update locations.js

--- assets/js/data/locations.js
+++ assets/js/data/locations.js
@@ -1,86 +1,123 @@
 var locations = [
-  {
-    key: "bergen-hohne",
-    title: "Bergen-Hohne",
-    region: "lower-saxony",
-    coords: [52.7819, 9.9273],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "fallingbostel",
-    title: "Fallingbostel",
-    region: "lower-saxony",
-    coords: [52.8664, 9.6956],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "verden",
-    title: "Verden",
-    region: "lower-saxony",
-    coords: [52.9348, 9.2330],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "soltau",
-    title: "Soltau",
-    region: "lower-saxony",
-    coords: [52.9869, 9.8434],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "celle",
-    title: "Celle",
-    region: "lower-saxony",
-    coords: [52.6226, 10.0805],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "nienburg",
-    title: "Nienburg",
-    region: "lower-saxony",
-    coords: [52.6461, 9.2205],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "hanover",
-    title: "Hanover (Hannover)",
-    region: "lower-saxony",
-    coords: [52.3759, 9.7320],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "braunschweig",
-    title: "Braunschweig",
-    region: "lower-saxony",
-    coords: [52.2689, 10.5268],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "hildesheim",
-    title: "Hildesheim",
-    region: "lower-saxony",
-    coords: [52.1508, 9.9510],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "luneburg",
-    title: "Luneburg",
-    region: "lower-saxony",
-    coords: [53.2468, 10.4115],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "hamburg",
-    title: "Hamburg",
-    region: "hamburg",
-    coords: [53.5511, 9.9937],
-    page: "locations/template-location.html"
-  },
-  {
-    key: "lubeck",
-    title: "Lubeck",
-    region: "schleswig-holstein",
-    coords: [53.8655, 10.6866],
-    page: "locations/template-location.html"
-  }
+
+{
+  key: "bergen-hohne",
+  title: "Bergen-Hohne",
+  region: "lower-saxony",
+  coords: [52.7819, 9.9273],
+  desc: "Major BAOR training area and armoured station.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "fallingbostel",
+  title: "Fallingbostel",
+  region: "lower-saxony",
+  coords: [52.8664, 9.6956],
+  desc: "BAOR garrison town associated with nearby training areas.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "verden",
+  title: "Verden",
+  region: "lower-saxony",
+  coords: [52.9348, 9.2330],
+  desc: "BAOR station town in Lower Saxony.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "soltau",
+  title: "Soltau",
+  region: "lower-saxony",
+  coords: [52.9869, 9.8434],
+  desc: "Station town connected with Soltau-Lüneburg training area.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "celle",
+  title: "Celle",
+  region: "lower-saxony",
+  coords: [52.6226, 10.0805],
+  desc: "BAOR station town and later Army Air Corps location.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "nienburg",
+  title: "Nienburg",
+  region: "lower-saxony",
+  coords: [52.6461, 9.2205],
+  desc: "BAOR garrison town on the Weser.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hanover",
+  title: "Hanover (Hannover)",
+  region: "lower-saxony",
+  coords: [52.3759, 9.7320],
+  desc: "Major city within the British Zone and BAOR administrative centre.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "braunschweig",
+  title: "Braunschweig",
+  region: "lower-saxony",
+  coords: [52.2689, 10.5268],
+  desc: "BAOR garrison city in Lower Saxony.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hildesheim",
+  title: "Hildesheim",
+  region: "lower-saxony",
+  coords: [52.1508, 9.9510],
+  desc: "BAOR station town south of Hannover.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "luneburg",
+  title: "Luneburg",
+  region: "lower-saxony",
+  coords: [53.2468, 10.4115],
+  desc: "BAOR garrison town near the Elbe.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hamburg",
+  title: "Hamburg",
+  region: "hamburg",
+  coords: [53.5511, 9.9937],
+  desc: "Major city within the British occupation zone.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "lubeck",
+  title: "Lubeck",
+  region: "schleswig-holstein",
+  coords: [53.8655, 10.6866],
+  desc: "British occupation zone port city near the inner German border.",
+  units: [],
+  page: "locations/template-location.html"
+}
+
 ];
