7f514755eea2a30adad52158843ea43dc8837ce3
kw554702-ux <kw554702@gmail.com>
2026-03-09 13:03:41 +0000

    Update locations.js

--- assets/js/data/locations.js
+++ assets/js/data/locations.js
@@ -229,5 +229,224 @@ var locations = [
   units: [],
   page: "locations/template-location.html"
 }
+{
+  key: "bad-oeynhausen",
+  title: "Bad Oeynhausen",
+  region: "nrw",
+  coords: [52.2066, 8.8037],
+  desc: "Post-war headquarters location of BAOR in North Rhine-Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
 
+{
+  key: "buckeburg",
+  title: "Bückeburg",
+  region: "lower-saxony",
+  coords: [52.2606, 9.0496],
+  desc: "BAOR station town later associated with the Army Air Corps.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "lemgo",
+  title: "Lemgo",
+  region: "nrw",
+  coords: [52.0273, 8.8995],
+  desc: "BAOR garrison town in North Rhine-Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "delmenhorst",
+  title: "Delmenhorst",
+  region: "lower-saxony",
+  coords: [53.0511, 8.6304],
+  desc: "British military location near Bremen.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hilden",
+  title: "Hilden",
+  region: "nrw",
+  coords: [51.1680, 6.9305],
+  desc: "British military location near Düsseldorf.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "monchengladbach",
+  title: "Mönchengladbach",
+  region: "nrw",
+  coords: [51.1805, 6.4428],
+  desc: "Major British military area near the Rhine.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "wolfenbuettel",
+  title: "Wolfenbüttel",
+  region: "lower-saxony",
+  coords: [52.1629, 10.5404],
+  desc: "BAOR location in Lower Saxony.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "iserlohn",
+  title: "Iserlohn",
+  region: "nrw",
+  coords: [51.3755, 7.7028],
+  desc: "BAOR garrison town in North Rhine-Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "dusseldorf",
+  title: "Düsseldorf",
+  region: "nrw",
+  coords: [51.2277, 6.7735],
+  desc: "Major city in the British Zone on the Rhine.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "dortmund",
+  title: "Dortmund",
+  region: "nrw",
+  coords: [51.5136, 7.4653],
+  desc: "Major industrial city in the British Zone.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "essen",
+  title: "Essen",
+  region: "nrw",
+  coords: [51.4556, 7.0116],
+  desc: "Industrial centre in the Ruhr within the British Zone.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "menden",
+  title: "Menden",
+  region: "nrw",
+  coords: [51.4380, 7.7950],
+  desc: "BAOR station town in North Rhine-Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hubblerath",
+  title: "Hubblerath",
+  region: "nrw",
+  coords: [51.2610, 6.8630],
+  desc: "British military site east of Düsseldorf.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "krefeld",
+  title: "Krefeld",
+  region: "nrw",
+  coords: [51.3388, 6.5853],
+  desc: "BAOR garrison city on the lower Rhine.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "duisburg",
+  title: "Duisburg",
+  region: "nrw",
+  coords: [51.4344, 6.7623],
+  desc: "Major Ruhr city within the British occupation zone.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "lubbecke",
+  title: "Lübbecke",
+  region: "nrw",
+  coords: [52.3064, 8.6170],
+  desc: "BAOR location in East Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "hameln",
+  title: "Hameln",
+  region: "lower-saxony",
+  coords: [52.1030, 9.3562],
+  desc: "BAOR garrison town on the River Weser.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "goslar",
+  title: "Goslar",
+  region: "lower-saxony",
+  coords: [51.9060, 10.4290],
+  desc: "British military location near the Harz region.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "lippstadt",
+  title: "Lippstadt",
+  region: "nrw",
+  coords: [51.6739, 8.3440],
+  desc: "BAOR garrison town in North Rhine-Westphalia.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "husum",
+  title: "Husum",
+  region: "schleswig-holstein",
+  coords: [54.4850, 9.0510],
+  desc: "British military location on the North Sea coast.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "itzehoe",
+  title: "Itzehoe",
+  region: "schleswig-holstein",
+  coords: [53.9270, 9.5160],
+  desc: "BAOR location in Schleswig-Holstein.",
+  units: [],
+  page: "locations/template-location.html"
+},
+
+{
+  key: "bad-lippspringe",
+  title: "Bad Lippspringe",
+  region: "nrw",
+  coords: [51.7875, 8.8166],
+  desc: "British military location near Paderborn.",
+  units: [],
+  page: "locations/template-location.html"
+}
 ];
+
