4091e80687a2f9d65abf3375eccc13d443f9c8d1
kw554702-ux <kw554702@gmail.com>
2026-03-10 10:29:31 +0000

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -13,7 +13,6 @@ if (loc.zoomTo) {
   marker.on("click", function(e) {
     map.setView(loc.zoomTo, loc.zoomLevel || 12);
   });
-}
 
 // Custom BAOR icon
 var baorIcon = L.icon({
@@ -126,5 +125,6 @@ L.control.layers(
   },
   { collapsed: false }
 ).addTo(map);
+
 
 
