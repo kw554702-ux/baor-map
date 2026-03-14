c0e7407ba9185e4cab1ea9087cb61d8962e08a3a
kw554702-ux <kw554702@gmail.com>
2026-03-14 15:04:14 +0000

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -165,74 +165,13 @@ if (targetKey) {
 } 
 
 
-// --- British Zone overlay layer ---
-var britishZoneLayer = L.geoJSON(null, {
-  style: function (feature) {
-    var p = feature.properties || {};
-    var rawName = p.name || p.NAME_1 || p.NAME || p.GEN || p.lan_name || '';
-    var name = String(rawName).toLowerCase();
 
-    var isBritishZone =
-      name.indexOf('schleswig') !== -1 ||
-      name.indexOf('hamburg') !== -1 ||
-      name.indexOf('niedersachsen') !== -1 ||
-      name.indexOf('lower saxony') !== -1 ||
-      name.indexOf('nordrhein') !== -1 ||
-      name.indexOf('north rhine') !== -1;
-
-    if (isBritishZone) {
-      return {
-        color: '#1f4aa8',
-        weight: 2,
-        opacity: 0.9,
-        fillColor: '#4f83ff',
-        fillOpacity: 0.15
-      };
-    }
-
-    return {
-      color: '#000000',
-      weight: 1,
-      opacity: 0,
-      fillOpacity: 0
-    };
-  },
-  onEachFeature: function (feature, layer) {
-    var p = feature.properties || {};
-    var rawName = p.name || p.NAME_1 || p.NAME || p.GEN || p.lan_name || 'Area';
-    var name = String(rawName).toLowerCase();
-
-    var isBritishZone =
-      name.indexOf('schleswig') !== -1 ||
-      name.indexOf('hamburg') !== -1 ||
-      name.indexOf('niedersachsen') !== -1 ||
-      name.indexOf('lower saxony') !== -1 ||
-      name.indexOf('nordrhein') !== -1 ||
-      name.indexOf('north rhine') !== -1;
-
-    if (isBritishZone) {
-      layer.bindPopup('<strong>' + rawName + '</strong><br>British Zone overlay');
-    }
-  }
-}).addTo(map);
-
-fetch('https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/2_bundeslaender/4_niedrig.geo.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(geojson) {
-    britishZoneLayer.addData(geojson);
-  })
-  .catch(function(err) {
-    console.log('Overlay failed to load:', err);
-  });
 
 // Layer switcher
 L.control.layers(
   null,
   {
-    'BAOR markers': markerLayer,
-    'British Zone overlay': britishZoneLayer
+    'BAOR markers': markerLayer
   },
   { collapsed: false }
 ).addTo(map);
